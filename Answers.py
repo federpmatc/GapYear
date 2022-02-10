0c024935456e771eb087ed6192d6182080842c64
federpmatc <61851250+federpmatc@users.noreply.github.com>
2022-02-10 11:25:30 -0600

    Update Answers.py

--- Answers.py
+++ Answers.py
@@ -1,43 +1,27 @@
 
 #Hello World
 from mcpi.minecraft import *
-
 mc = Minecraft.create()
 mc.postToChat("Hello  World")
-
-
-
-import mcpi.minecraft as Minecraft
-from House_Complete import *
-
-mc = Minecraft.create()
-x,y,z = mc.player.getTilePos()
-house(x+10,y,z)
 
 #Teleport
 from mcpi.minecraft import *
-
 mc = Minecraft.create()
 mc.postToChat("Hello  World")
-
 x,y,z = mc.player.getPos()
 mc.player.setPos(x-5,y+50,z)
 
 #Set Block
 from mcpi.minecraft import *
-
 mc = Minecraft.create()
 mc.postToChat("Hello  World")
-
 x,y,z = mc.player.getPos()
 mc.setBlock(x+1,y,z,1)
 
 #Creating a Column
 from mcpi.minecraft import *
-
 mc = Minecraft.create()
 mc.postToChat("Hello  World")
-
 x,y,z = mc.player.getPos()
 mc.setBlock(x+3,y,z,1)
 mc.setBlock(x+3,y+1,z,1)
@@ -46,10 +30,8 @@ mc.setBlock(x+3,y+2,z,1)
 #Block Constants
 from mcpi.minecraft import *
 from mcpi import block
-
 mc = Minecraft.create()
 mc.postToChat("Hello  World")
-
 x,y,z = mc.player.getPos()
 mc.setBlock(x+3,y,z,block._BLOCK.id)
 
@@ -59,7 +41,6 @@ from mcpi import block
 
 mc = Minecraft.create()
 mc.postToChat("Hello  World")
-
 x,y,z = mc.player.getPos()
 dirt=3
 mc.setBlock(x+1,y,z,dirt)
@@ -70,7 +51,6 @@ from mcpi import block
 
 mc = Minecraft.create()
 mc.postToChat("Hello  World")
-
 x,y,z = mc.player.getPos()
 wool=35
 mc.setBlock(x+1,y,z,wool,1)
@@ -78,21 +58,17 @@ mc.setBlock(x+1,y,z,wool,1)
 #Set Multiple Blocks
 from mcpi.minecraft import *
 from mcpi import block
-
 mc = Minecraft.create()
 mc.postToChat("Hello  World")
-
-x,yz = mc.player.getPos()
+x,y,z = mc.player.getPos()
 stone=1
 mc.setBlocks(x+1,y+1,z+1,x+11,y+11,z+11,stone)
 
 #Fun with TNT
 from mcpi.minecraft import *
 from mcpi import block
-
 mc = Minecraft.create()
 mc.postToChat("Hello  World")
-
 x,y,z = mc.player.getPos()
 tnt=46
 mc.setBlocks(x+1,y+1,z+1,x+11,y+11,z+11,tnt,1)
@@ -100,9 +76,7 @@ mc.setBlocks(x+1,y+1,z+1,x+11,y+11,z+11,tnt,1)
 #Fun with Lava
 from mcpi.minecraft import *
 from mcpi import block
-
 mc = Minecraft.create()
-
 x,y,z = mc.player.getPos()
 lava=10
 mc.setBlock(x+3,y+3,z+3,lava)
@@ -110,15 +84,11 @@ mc.setBlock(x+3,y+3,z+3,lava)
 #More Fun with Lava
 from mcpi import block
 from time import sleep
-
 mc = Minecraft.create()
-
 x,y,z = mc.player.getPos()
-
 lava=10
 water=8
 air=0
-
 mc.setBlock(x+3,y+3,z+3,lava)
 sleep(3)
 mc.setBlock(x+3,y+3,z+3,water)
@@ -128,22 +98,16 @@ mc.setBlock(x+3,y+3,z+3,air)
 #Make a house
 from mcpi.minecraft import *
 from House_Complete import *
-
 mc = Minecraft.create()
-
 x,y,z = mc.player.getPos()
-
 house(x+10,y,z)
 
 
 #Make 10 houses
 from mcpi.minecraft import *
 from House_Complete import *
-
 mc = Minecraft.create()
-
 x,y,z = mc.player.getPos()
-
 for number in range(10,100,10):
     print(number)
     house(x+number,y,z)
